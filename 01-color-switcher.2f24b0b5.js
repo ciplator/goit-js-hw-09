f0380fc0e6b478280c204c8117bcc298333b8c60
ciplator <ciplator@gmail.com>
2023-09-07 18:55:01 +0000

    Deploying to gh-pages from @ ciplator/goit-js-hw-09@f647f86771a64681a1d212ec6984b38d174c6619 🚀

---
+++ 01-color-switcher.2f24b0b5.js
@@ -0,0 +1,2 @@
+let e=null;const t=document.getElementById("Start"),d=document.getElementById("Stop"),l=document.body;t.addEventListener("click",(()=>{t.disabled=!0,d.disabled=!1,e=setInterval((()=>{l.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}),1e3)})),d.addEventListener("click",(()=>{t.disabled=!1,d.disabled=!0,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.2f24b0b5.js.map
---
+++ 01-color-switcher.7288fd54.js
@@ -0,0 +1,2 @@
+!function(){var t=null,e=document.getElementById("Start"),n=document.getElementById("Stop"),d=document.body;e.addEventListener("click",(function(){e.disabled=!0,n.disabled=!1,t=setInterval((function(){d.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),n.addEventListener("click",(function(){e.disabled=!1,n.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.7288fd54.js.map
